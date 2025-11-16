927872c49fdd0ecf1052741d1cf6f0730f944c96
NIKHIL VENKAT KOLLI <nikhilvenkat0@gmail.com>
2025-11-17 01:10:38 +0530

    Update RootController.java

--- BackEnd/src/main/java/com/FSP/BackEnd/controller/RootController.java
+++ BackEnd/src/main/java/com/FSP/BackEnd/controller/RootController.java
@@ -1,11 +1,19 @@
 package com.FSP.BackEnd.controller;
 
+import com.FSP.BackEnd.repository.FileRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RootController {
+
+    private final FileRepository fileRepository;
+
+    // Constructor injection
+    public RootController(FileRepository fileRepository) {
+        this.fileRepository = fileRepository;
+    }
 
     @GetMapping("/")
     public ResponseEntity<String> healthCheck() {
